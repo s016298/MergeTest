a459d1813aeb427869eddc1f4aea517fda092bab
Justin Cannan (student LM) <s016298@lm-20s016298.lmsd.org>
2019-02-04 13:05:17 -0500

    Initial Commit

---
+++ Segue Between Thingies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segue Between Thingies
+//
+//  Created by Justin Cannan (student LM) on 2/4/19.
+//  Copyright © 2019 Lord Kangron's Feast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Segue Between ThingiesTests/Segue_Between_ThingiesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Segue_Between_ThingiesTests.swift
+//  Segue Between ThingiesTests
+//
+//  Created by Justin Cannan (student LM) on 2/4/19.
+//  Copyright © 2019 Lord Kangron's Feast. All rights reserved.
+//
+
+import XCTest
+@testable import Segue_Between_Thingies
+
+class Segue_Between_ThingiesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
